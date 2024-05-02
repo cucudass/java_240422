667d6286e87d5fc1a7665c711ec9acbaef7d5ed5
조광래 <whrhkdfo12@naver.com>
2024-05-02 23:30:21 +0900

    연습 문제 배열 이용

--- java_240422/src/question04/ChangeMoney.java
+++ java_240422/src/question04/ChangeMoney.java
@@ -4,50 +4,19 @@ import java.util.Scanner;
 
 public class ChangeMoney {
 	public static void main(String[] args) {
+		int[] marr = {50000,10000,1000,500,100,50,10,1};
+		String[][] mstr = {{"오만원권","매"},{"만원권","매"},{"천원권","매"},{"오백원","개"},{"백원","개"},{"오십원","개"},{"십원","개"},{"일원","개"}};
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.print("금액을 입력하시오>>");
 		int money = sc.nextInt();
-		
-		int cnt = money / 50000;
-		if(cnt > 0) 
-			System.out.println("오만원권 " + cnt + "매");
-		
-		money %= 50000;
-		cnt = money / 10000;
-		if(cnt > 0)
-			System.out.println("만원권 " + cnt + "매");
-		
-		money %= 10000;
-		cnt = money / 1000;
-		if(cnt > 0)
-			System.out.println("천원권 " + cnt + "매");
-		
-		money %= 1000;
-		cnt = money / 500;
-		if(cnt > 0)
-			System.out.println("오백원 " + cnt + "개");
-		
-		money %= 500;
-		cnt = money / 100;
-		if(cnt > 0)
-			System.out.println("백원 " + cnt + "개");
-		
-		money %= 100;
-		cnt = money / 50;
-		if(cnt > 0)
-			System.out.println("오십원 " + cnt + "개");
-		
-		money %= 50;
-		cnt = money / 10;
-		if(cnt > 0)
-			System.out.println("십원 " + cnt + "개");
-		
-		money %= 10;
-		cnt = money / 1;
-		if(cnt > 0)
-			System.out.println("일원 " + cnt + "개");
-			
+		int cnt = 0;
+		for(int i = 0; i < marr.length; i++) {
+		    cnt = money / marr[i];
+		    if(cnt > 0)
+		    	System.out.println(mstr[i][0] + " "+ cnt + mstr[i][1]);
+		    money %= marr[i];
+		}
 		sc.close();
-		
 	}
 }
